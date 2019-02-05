bac40c5218713085506d5b387fc56ce32529d7ec
james <james-po@is.naist.jp>
2019-02-05 12:43:52 +0900

    initial commit

---
+++ src/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import rospy
+from motive_ur_calib.srv import *
+
+rospy.init_node('motive_ur_calib_test', anonymous=True)
+
+rospy.wait_for_service('motiveTF')
+svc_ = rospy.ServiceProxy('motiveTF', MotiveTF)
+
+### test point TF 
+
+#from solve_tf import motive_samples, ur_marker_positions
+#from tf_node import xN_arr_to_ROSPts
+
+#req_ = MotiveTFRequest()
+#req_.markers = xN_arr_to_ROSPts(motive_samples.T)
+#res_ = svc_(req_).markers
+
+#import numpy as np
+
+#n_ = len(res_); hypot_ = np.empty(n_)
+#for i in range(n_):
+#    resp_ = [ res_[i].x, res_[i].y, res_[i].z ]
+#    hypot_[i] = np.sum(np.square(ur_marker_positions[i] - resp_))
+#hypot_ = np.sqrt(hypot_)
+
+#print('  Mean error: %f' % np.mean(hypot_))
+#print('  Max error: %f' % np.max(hypot_))
+
+## test pose TF
+
+from geometry_msgs.msg import PoseStamped, TransformStamped
+from tf2_ros import TransformBroadcaster
+
+tfbc_ = TransformBroadcaster()
+
+def broadcast_pose_TF(pose, parent_frame='ur_base_link', child_frame='tf_test'):
+    t = TransformStamped()
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = parent_frame
+    t.child_frame_id = child_frame
+    t.transform.translation = pose.position
+    t.transform.rotation = pose.orientation
+    tfbc_.sendTransform(t)
+    
+rigid_body_name_ = 'gear_big'
+rigid_body_pose_topic_ = '/natnet_client/rigid_bodies/' + rigid_body_name_ + '/pose'
+
+while True:
+    msg_ = rospy.wait_for_message(rigid_body_pose_topic_, PoseStamped).pose
+    req_ = MotiveTFRequest(rigid_bodies = [ msg_ ])
+    rigid_body_pose_tf_ = svc_(req_).rigid_bodies[0]
+    broadcast_pose_TF(rigid_body_pose_tf_, child_frame=rigid_body_name_+'_TF')
---
+++ src/tf_node.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import rospy, tf_conversions
+from motive_ur_calib.srv import *
+from geometry_msgs.msg import Point, Quaternion, Pose
+
+import numpy as np
+
+def ROSPt_to_4x1_arr(P):
+    return np.asarray([ P.x,P.y, P.z, 1.0]).reshape([4,1])
+
+def ROSPts_to_4xN_arr(P):
+    n_ = len(P); M_ = np.empty([4,n_])
+    for i in range(n_): M_[:,i] = ROSPt_to_4x1_arr(P[i]).ravel()
+    
+    return M_
+    
+def x1_arr_to_ROSPt(M):
+    return Point(x=M[0], y=M[1], z=M[2])
+
+def xN_arr_to_ROSPts(M):
+    P_ = []
+    for i in range(M.shape[1]):
+        P_.append(x1_arr_to_ROSPt(M[:,i]))
+           
+    return P_
+    
+def ROSQuat_to_xyzw(q):
+    return [ q.x, q.y, q.z, q.w ]
+    
+def ROSPose_to_TM(pose):
+    f_ = tf_conversions.posemath.fromMsg(pose)
+    return tf_conversions.posemath.toMatrix(f_)
+    
+def TM_to_ROSPose(M):
+    f_ = tf_conversions.posemath.fromMatrix(M)
+    return tf_conversions.posemath.toMsg(f_)
+
+class MotiveTF_node():
+    def __init__(self):
+        rospy.init_node('motiveTF_node', anonymous=True)
+        
+        htm_list_ = rospy.get_param("~htm").split(',')
+        self._htm = np.asarray(map(float,htm_list_)).reshape(4,4)
+
+        self._rb_rot_adj = tf_conversions.transformations.quaternion_from_matrix(self._htm)
+        self._rb_rot_adj[3] = -self._rb_rot_adj[3] # invert by taking w = -w
+        
+        rospy.Service('motiveTF', MotiveTF, self._tf_cb)
+        
+        rospy.spin()
+        
+    def _tf_cb(self, req):
+        res_ = MotiveTFResponse()
+        
+        # marker positions
+        res_.markers = xN_arr_to_ROSPts(
+            np.matmul(self._htm, ROSPts_to_4xN_arr(req.markers)))
+        
+        # rigid body poses
+        n_ = len(req.rigid_bodies); res_.rigid_bodies = []
+        for i in range(n_):
+            quat_rot_adj_ = tf_conversions.transformations.quaternion_multiply(
+                ROSQuat_to_xyzw(req.rigid_bodies[i].orientation), self._rb_rot_adj)
+            req_pose_rot_adj_ = Pose(position=req.rigid_bodies[i].position,
+                orientation=Quaternion(*quat_rot_adj_))           
+            M_tf_ = np.matmul(self._htm, ROSPose_to_TM(req_pose_rot_adj_))
+            res_.rigid_bodies.append(TM_to_ROSPose(M_tf_))
+    
+        return res_
+        
+if __name__ == '__main__':
+    mTF_node_ = MotiveTF_node()
